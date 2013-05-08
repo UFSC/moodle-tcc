c55015233d41aba74929c787c246fc0c31547363
Bruno Silveira <bruno.silveira@setic.ufsc.br>
2013-05-08 16:20:48 -0300

    Modificação no application.rb para modificar o gerador do rails.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,11 @@ module SistemaTcc
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
+    config.generators do |g|
+      g.test_framework      :rspec, fixture: true
+      g.fixture_replacement :fabrication
+    end
   end
 end
