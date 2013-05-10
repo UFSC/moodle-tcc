920cceb40c30c8a69e2e82a199578cd64de0b4c0
Gabriel Mazetto <brodock@gmail.com>
2013-05-10 18:53:14 -0300

    Personalização do editor (removendo botões inúteis)

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -2,12 +2,9 @@ CKEDITOR.editorConfig = function( config ) {
     config.toolbarGroups = [
         { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
         { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-        { name: 'insert' },
-        { name: 'document',    groups: [ 'mode', 'document', 'doctools' ] },
         { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
         { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
         { name: 'styles' },
-        { name: 'colors' },
         { name: 'tools' },
     ]
     config.extraPlugins = 'citacao'
