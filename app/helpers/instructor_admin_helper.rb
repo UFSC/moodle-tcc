8a4876d74d355728bdd24dcd009f8679325a9ba9
fabioariati@inf.ufsc.br <fabiocariati@gmail.com>
2013-05-17 19:53:02 -0300

    tela inicial de tutor

--- app/helpers/instructor_admin_helper.rb
+++ app/helpers/instructor_admin_helper.rb
@@ -1,2 +1,10 @@
 module InstructorAdminHelper
+  def get_state(tcc, category)
+    #tcc.hubs.where(category: category).first.state
+    "teste state"
+  end
+
+  def get_action(tcc, category)
+    "teste action"
+  end
 end
