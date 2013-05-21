cddf5b061223476f3485ae5afaa7381aafb7b1b5
Gabriel Mazetto <brodock@gmail.com>
2013-05-21 02:35:53 -0300

    Melhoria de deploy (acelera o assets precompile)

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,9 @@ module SistemaTcc
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # Não inicializa o rails para compilar os assets
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
