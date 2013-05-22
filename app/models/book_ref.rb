3493b75f3f8e6d078446167a6949730c03e97669
Gabriel Mazetto <brodock@gmail.com>
2013-05-22 17:21:35 -0300

    Correções nas validações e testes

--- app/models/book_ref.rb
+++ app/models/book_ref.rb
@@ -2,9 +2,9 @@ class BookRef < ActiveRecord::Base
 
   validates_presence_of :first_author, :edition_number, :local, :year, :title, :publisher
   validates_presence_of :num_quantity, :if => :type_quantity
-  validates :type_quantity, :inclusion => %w(p, ed)
+  validates :type_quantity, :inclusion => { :in => %w(p ed) }, :allow_blank => true
   validates :year, :numericality => { :only_integer => true }
-  validates :year, :inclusion => { :in => %w(0,->{Date.today.year}) }
+  validates :year, :inclusion => { :in => lambda{ |book| 0..Date.today.year } }
 
   attr_accessible :first_author, :edition_number, :et_all, :local, :num_quantity, :publisher, :second_author, :subtitle, :third_author, :title, :type_quantity, :year
 
--- spec/fabricators/book_ref.rb
+++ spec/fabricators/book_ref.rb
@@ -1,14 +1,25 @@
+# encoding: utf-8
 Fabricator(:book_ref) do
   first_author 'Juca bala'
-  second_author
-  third_author
   et_all false
   title 'Título'
-  subtitle
   edition_number 1
   local 'Cidade'
   publisher 'Editora'
   year 1990
-  type_quantity
-  num_quantity
+end
+
+Fabricator(:book_ref_full, from: :book_ref) do
+  first_author { Faker::Name.name }
+  second_author { Faker::Name.name }
+  third_author { Faker::Name.name }
+  et_all false
+  title 'Título'
+  subtitle 'Subtítulo'
+  edition_number 1
+  local 'Cidade'
+  publisher 'Editora'
+  year 1990
+  type_quantity 'p'
+  num_quantity 235
 end
