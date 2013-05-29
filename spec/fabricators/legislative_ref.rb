4dd951c144c08a012441738f8cfe9548ba1fe6e7
Gabriel Mazetto <brodock@gmail.com>
2013-05-29 17:00:16 -0300

    Melhorias nos testes de referência do legislativo

--- spec/fabricators/legislative_ref.rb
+++ spec/fabricators/legislative_ref.rb
@@ -1,9 +1,9 @@
 Fabricator(:legislative_ref) do
-  jurisdiction_or_header "MyString"
-  title                  "MyString"
-  edition                "MyString"
-  local                  "MyString"
-  publisher              "MyString"
-  year                   1
-  total_pages            1
+  jurisdiction_or_header { Faker::Lorem.sentence(3) }
+  title                  { Faker::Lorem.sentence(4) }
+  edition                { 1..10.sample }
+  local                  { Faker::Address.city }
+  publisher              { Faker::Company.name }
+  year                   2003
+  total_pages            { 1..250.sample }
 end
--- spec/models/legislative_ref_spec.rb
+++ spec/models/legislative_ref_spec.rb
@@ -11,8 +11,30 @@ describe LegislativeRef do
     it { should validate_presence_of(:edition) }
     it { should validate_presence_of(:local) }
     it { should validate_presence_of(:publisher) }
-    it { should validate_presence_of(:year) }
     it { should validate_presence_of(:total_pages) }
+  end
 
+  describe '#edition' do
+    it { should validate_numericality_of(:edition).only_integer }
+    it { should_not allow_value(-1).for(:edition) }
+    it { should_not allow_value(-5).for(:edition) }
+    it { should_not allow_value(0).for(:edition) }
+    it { should allow_value(1).for(:edition) }
+    it { should allow_value(5).for(:edition) }
+  end
+
+  describe '#total_pages' do
+    it { should validate_numericality_of(:total_pages).only_integer }
+    it { should_not allow_value(-1).for(:total_pages) }
+    it { should_not allow_value(-5).for(:total_pages) }
+    it { should_not allow_value(0).for(:total_pages) }
+    it { should allow_value(1).for(:total_pages) }
+    it { should allow_value(5).for(:total_pages) }
+  end
+
+  describe '#year' do
+    it { should validate_presence_of(:year) }
+    it { should validate_numericality_of(:year).only_integer }
+    it { should ensure_inclusion_of(:year).in_range(0..(Date.today.year)) }
   end
 end
