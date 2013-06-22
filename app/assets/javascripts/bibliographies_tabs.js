4e83a6ccd1a756bbb66829ce1891c6c6e20c56b8
Bruno Silveira <bruno.silveira@setic.ufsc.br>
2013-06-22 14:40:36 -0300

    Correçao.

--- app/assets/javascripts/bibliographies_tabs.js
+++ app/assets/javascripts/bibliographies_tabs.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
+    var hash = window.location.hash;
 
     $("ul#tabs li").removeClass("active"); //Remove any "active" class
     $('.tab-content').addClass('hidden'); //Hide all tab content
 
     var tabs = $("ul#tabs li");
-    var hash = window.location.hash;
+
     var component = tabs.find('[href=' + hash + ']').parent();
 
     component.addClass('active');
     $('#content-' + component.attr('id')).removeClass('hidden');
+    if(hash == ''){
+        $('#geral').addClass('active');
+        $('#content-geral').removeClass('hidden');
+    }
+
     tabs.click(function () {
         $('#tabs').find('li').each(function () {
             if ($(this).hasClass('active')) {
