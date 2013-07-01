31b60d5fee76900494a824914cb85c1f5ad1bd90
fabioariati <fabiocariati@gmail.com>
2013-07-01 17:21:20 -0300

    Mostrando título do diário

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,8 +2,6 @@ class Diary < ActiveRecord::Base
   belongs_to :hub
   belongs_to :diary_definition
 
-  attr_accessible :content, :pos, :title, :diary_definition, :hub
+  attr_accessible :content, :pos, :diary_definition, :hub
 
-   # Virtual attribute
-  attr_accessor :title
 end
