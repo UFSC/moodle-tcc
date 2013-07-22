6ad5c7a5f22a8384769cca8a786e475954f7afb6
fabioariati <fabiocariati@gmail.com>
2013-07-22 18:15:54 -0300

    Correção no teste

--- spec/requests/service_spec.rb
+++ spec/requests/service_spec.rb
@@ -3,15 +3,11 @@ require 'spec_helper'
 
 describe "Service" do
   describe "POST /reportingservice" do
-    before(:all) do
-      @host = 'http://127.0.0.1:3000/reportingservice'
-    end
-
     it 'should render error with invalid consumer key' do
       expected = {
           error_message: 'Invalid consumer key'
       }.to_json
-      response = RestClient.post @host, consumer_key: 'wrong_key', user_ids: [2,4]
+      post reportingservice_path, { consumer_key: 'wrong_key', user_ids: [2,4] }
       response.body.should == expected
     end
 
@@ -19,7 +15,7 @@ describe "Service" do
       expected = {
           error_message: 'Invalid params (missing user_ids)'
       }.to_json
-      response = RestClient.post @host, consumer_key: 'consumer_key'
+      post reportingservice_path, { consumer_key: 'consumer_key' }
       response.body.should == expected
     end
   end
