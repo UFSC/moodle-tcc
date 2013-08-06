c2fc100c20d7032f3ab83c7663d949b1de4454d2
Gabriel Mazetto <brodock@gmail.com>
2013-08-06 17:23:56 -0300

    Correção na migração para contemplar todos os casos
    Deve ignorar quando o publication_date for nil (evita chamada em nil)
    Deve marcar corretamente que não é possível desfazer essa migração

--- db/migrate/20130722124459_change_article_refs_publication_date.rb
+++ db/migrate/20130722124459_change_article_refs_publication_date.rb
@@ -1,19 +1,21 @@
 class ChangeArticleRefsPublicationDate < ActiveRecord::Migration
   def up
-    add_column :article_refs, :year, :integer
+    add_column :article_refs, :year, :integer, :after => :publication_date
 
     ArticleRef.reset_column_information
 
     article_refs = ArticleRef.all
     article_refs.each do |a|
-      a.year = a.publication_date.year
-      a.save
+      unless a.publication_date.nil?
+        a.year = a.publication_date.year
+        a.save
+      end
     end
 
     remove_column :article_refs, :publication_date
   end
 
   def down
-    # Nada
+    raise ActiveRecord::IrreversibleMigration
   end
 end
