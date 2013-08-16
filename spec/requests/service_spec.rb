ed3c06bfc36ae645c0ef4614691a38366825ecf4
fabioariati <fabiocariati@gmail.com>
2013-08-16 16:01:51 -0300

    Adicionado gem 'json_sper' e spec de request faltante

--- spec/requests/service_spec.rb
+++ spec/requests/service_spec.rb
@@ -4,19 +4,25 @@ require 'spec_helper'
 describe "Service" do
   describe "POST /reportingservice" do
     it 'should render error with invalid consumer key' do
-      expected = {
-          error_message: 'Invalid consumer key'
-      }.to_json
       post reportingservice_path, { consumer_key: 'wrong_key', user_ids: [2,4] }
-      response.body.should == expected
+      response.body.should have_json_path('error_message')
     end
 
     it 'should render error without user_ids' do
-      expected = {
-          error_message: 'Invalid params (missing user_ids)'
-      }.to_json
       post reportingservice_path, { consumer_key: 'consumer_key' }
-      response.body.should == expected
+      response.body.should have_json_path('error_message')
+    end
+  end
+
+  describe "POST /tcc_definition_service" do
+    it 'should render error with invalid consumer key' do
+      post tcc_definition_service_path, { consumer_key: 'wrong_key', user_ids: [2,4] }
+      response.body.should have_json_path('error_message')
+    end
+
+    it 'should render error without user_ids' do
+      post tcc_definition_service_path, { consumer_key: 'consumer_key' }
+      response.body.should have_json_path('error_message')
     end
   end
 end
