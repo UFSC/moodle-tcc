57b7d19086e52f5514db6258a1d1ef84c870a0f0
fabioariati <fabiocariati@gmail.com>
2013-09-12 17:40:43 -0300

    Vinculando eixo na tarefa de LTI

--- lib/modules/moodle_grade.rb
+++ lib/modules/moodle_grade.rb
@@ -1,11 +1,13 @@
 module MoodleGrade
   # Cria um item de nota para o curso especificado e com o nome determinado
-  def self.populate_grade_items(courseid, itemname)
+  def self.populate_grade_items(courseid, itemname, lti_id, itemnumber)
     Rails.logger.debug "[WS Moodle] Acessando Web Service create_grade_item: itemname=#{itemname}, course_id=#{courseid}"
     RestClient.post(TCC_CONFIG['server'],
                     :wsfunction => 'local_wstcc_create_grade_item',
                     :courseid => courseid,
                     :itemname => itemname,
+                    :lti_id => lti_id,
+                    :itemnumber => itemnumber,
                     :wstoken => TCC_CONFIG['token']) do |response|
       MooddleWsClient.check_error(response)
       response
