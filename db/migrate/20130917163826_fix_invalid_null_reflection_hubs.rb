61eb5ac63d571975eaabe90e374d8d1d9f160031
Gabriel Mazetto <brodock@gmail.com>
2013-09-17 14:16:48 -0300

    Migration responsável por corrigir transicionamento de hubs inválidos

---
+++ db/migrate/20130917163826_fix_invalid_null_reflection_hubs.rb
@@ -0,0 +1,9 @@
+class FixInvalidNullReflectionHubs < ActiveRecord::Migration
+  def change
+    hubs = Hub.where("reflection = '' AND (state != 'draft' AND state != 'new')")
+    hubs.with_progress 'Corrigindo hubs com transição incorreta sem reflexão' do |hub|
+      hub.state = 'draft'
+      hub.save!
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130913210759) do
+ActiveRecord::Schema.define(:version => 20130917163826) do
 
   create_table "abstracts", :force => true do |t|
     t.text     "content",    :limit => 16777215
