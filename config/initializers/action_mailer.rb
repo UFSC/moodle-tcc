44540d1d8d86e5a2fe0819b5af33e6b7b105e9a5
Gabriel Mazetto <brodock@gmail.com>
2013-10-02 18:47:12 -0300

    Alterado mecanismo de configuração de e-mail.
    Foi baseado no código do Redmine para permitir selecionar o método
    de envio.
    
    Esse código faz algumas validações para garantir o preenchimento do arquivo.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,13 +1,28 @@
-module ActionMailer
-  class Base
-    cattr_accessor :smtp_config
+# encoding: utf-8
+module Configuration
+  def self.load!
+    filename = File.join(Rails.root, 'config', 'email.yml')
 
-    self.smtp_config = YAML::load(File.open("#{Rails.root}/config/smtp.yml"))[Rails.env]
+    unless File.file?(filename)
+      $stderr.puts 'Você precisa configurar corretamente os dados de e-mail. Verifique o arquivo email.yml.example'
+      exit 1
+    end
 
-    def self.smtp_settings
-      mailer = self.smtp_config[mailer_name]
-      @@smtp_settings = mailer.symbolize_keys
+    begin
+      email_config = YAML::load_file(filename)[Rails.env]
+    rescue ArgumentError
+      $stderr.puts "Seu arquivo de configuração de e-mail, localizado em #{filename} não é um arquivo YAML válido."
+      exit 1
+    end
+
+    ActionMailer::Base.perform_deliveries = true if email_config['delivery_method']
+
+    email_config.each do |k, v|
+      v.symbolize_keys! if v.respond_to?(:symbolize_keys!)
+      ActionMailer::Base.send("#{k}=", v)
     end
 
   end
 end
+
+Configuration.load!
