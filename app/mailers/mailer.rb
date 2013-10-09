327caab9387bc8ba51224f1ba1d716cdb4f6168b
Gabriel Mazetto <brodock@gmail.com>
2013-10-09 18:38:48 -0300

    Correção no Mailer pra utilizar corretamente a configuração do FROM

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class Mailer < ActionMailer::Base
-  default from: Proc.new { TCC_CONFIG['notification_email'] }
+  default from: -> { TCC_CONFIG['notification_email'] }
 
   def state_altered(email, old_state, new_state, url)
     @old = old_state
