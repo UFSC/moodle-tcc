5c2f3888005c28dae18c16ea88bd605a78b60f98
Bruno Silveira <bruno.silveira@setic.ufsc.br>
2013-10-09 09:43:19 -0300

    Adição de testes com os tipos de referencias restantes.

--- spec/libs/conversor_spec.rb
+++ spec/libs/conversor_spec.rb
@@ -6,16 +6,348 @@ describe Conversor do
     let(:tcc) { Fabricate.build(:tcc) }
 
     describe 'article_refs' do
+      it 'should convert citacao direta' do
+        ref = Fabricate(:article_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
 
+        title = ref.article_title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.article_title}]]")
+        text = Conversor::convert_text(citacao, tcc)
+
+        text.rstrip.should == citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)
+
+      end
+      it 'should convert citacao indireta' do
+        ref = Fabricate(:article_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
+
+        title = ref.article_title
+        citacao_type = "ci"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.article_title}]]")
+        text = Conversor::convert_text(citacao, tcc)
+
+        text.rstrip.should == citacao(title, citacao_type, class_type, ref_id, ref.indirect_citation)
+      end
+      it 'should just convert citacao' do
+        ref = Fabricate(:article_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
+
+        title = ref.article_title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+
+        prefix = Faker::Lorem.paragraph(1)
+        sufix = Faker::Lorem.paragraph(1)
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.article_title}]]")
+        text = prefix+citacao+sufix
+
+        text = Conversor::convert_text(text, tcc)
+        text.rstrip.should == prefix+citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)+sufix
+
+      end
+
+      it 'should convert all citacao from text' do
+        ref = Fabricate(:article_ref)
+        ref1 = Fabricate(:article_ref)
+
+        tcc.save!
+        tcc.references.create!(element: ref)
+        tcc.references.create!(element: ref1)
+
+        title = ref.article_title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.article_title}]]")
+
+        title1 = ref1.article_title
+        citacao_type1 = "cd"
+        class_type1 = Conversor::REFERENCES_TYPE.invert[ref1.class.to_s]
+        ref_id1 = ref1.id
+
+        citacao1 = citacao(title1, citacao_type1, class_type1, ref_id1, "[[#{Conversor::REFERENCES_TYPE.invert[ref1.class.to_s]}#{ref1.id} #{ref1.article_title}]]")
+
+        prefix = Faker::Lorem.paragraph(1)
+        sufix = Faker::Lorem.paragraph(1)
+
+        text = prefix+citacao+sufix+citacao1
+
+        text = Conversor::convert_text(text, tcc)
+        text.rstrip.should == prefix+citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)+sufix+citacao(title1, citacao_type1, class_type1, ref_id1, ref1.direct_citation)
+
+      end
     end
     describe 'book_cap_refs' do
+      it 'should convert citacao direta' do
+        ref = Fabricate(:book_cap_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
 
+        title = ref.book_title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.book_title}]]")
+        text = Conversor::convert_text(citacao, tcc)
+
+        text.rstrip.should == citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)
+
+      end
+      it 'should convert citacao indireta' do
+        ref = Fabricate(:book_cap_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
+
+        title = ref.book_title
+        citacao_type = "ci"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.book_title}]]")
+        text = Conversor::convert_text(citacao, tcc)
+
+        text.rstrip.should == citacao(title, citacao_type, class_type, ref_id, ref.indirect_citation)
+      end
+      it 'should just convert citacao' do
+        ref = Fabricate(:book_cap_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
+
+        title = ref.book_title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+
+        prefix = Faker::Lorem.paragraph(1)
+        sufix = Faker::Lorem.paragraph(1)
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.book_title}]]")
+        text = prefix+citacao+sufix
+
+        text = Conversor::convert_text(text, tcc)
+        text.rstrip.should == prefix+citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)+sufix
+
+      end
+
+      it 'should convert all citacao from text' do
+        ref = Fabricate(:book_cap_ref)
+        ref1 = Fabricate(:book_cap_ref)
+
+        tcc.save!
+        tcc.references.create!(element: ref)
+        tcc.references.create!(element: ref1)
+
+        title = ref.book_title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.book_title}]]")
+
+        title1 = ref1.book_title
+        citacao_type1 = "cd"
+        class_type1 = Conversor::REFERENCES_TYPE.invert[ref1.class.to_s]
+        ref_id1 = ref1.id
+
+        citacao1 = citacao(title1, citacao_type1, class_type1, ref_id1, "[[#{Conversor::REFERENCES_TYPE.invert[ref1.class.to_s]}#{ref1.id} #{ref1.book_title}]]")
+
+        prefix = Faker::Lorem.paragraph(1)
+        sufix = Faker::Lorem.paragraph(1)
+
+        text = prefix+citacao+sufix+citacao1
+
+        text = Conversor::convert_text(text, tcc)
+        text.rstrip.should == prefix+citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)+sufix+citacao(title1, citacao_type1, class_type1, ref_id1, ref1.direct_citation)
+
+      end
     end
     describe 'book_refs' do
+      it 'should convert citacao direta' do
+        ref = Fabricate(:book_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
 
+        title = ref.title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.title}]]")
+        text = Conversor::convert_text(citacao, tcc)
+
+        text.rstrip.should == citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)
+
+      end
+      it 'should convert citacao indireta' do
+        ref = Fabricate(:book_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
+
+        title = ref.title
+        citacao_type = "ci"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.title}]]")
+        text = Conversor::convert_text(citacao, tcc)
+
+        text.rstrip.should == citacao(title, citacao_type, class_type, ref_id, ref.indirect_citation)
+      end
+      it 'should just convert citacao' do
+        ref = Fabricate(:book_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
+
+        title = ref.title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+
+        prefix = Faker::Lorem.paragraph(1)
+        sufix = Faker::Lorem.paragraph(1)
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.title}]]")
+        text = prefix+citacao+sufix
+
+        text = Conversor::convert_text(text, tcc)
+        text.rstrip.should == prefix+citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)+sufix
+
+      end
+
+      it 'should convert all citacao from text' do
+        ref = Fabricate(:book_ref)
+        ref1 = Fabricate(:book_ref)
+
+        tcc.save!
+        tcc.references.create!(element: ref)
+        tcc.references.create!(element: ref1)
+
+        title = ref.title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.title}]]")
+
+        title1 = ref1.title
+        citacao_type1 = "cd"
+        class_type1 = Conversor::REFERENCES_TYPE.invert[ref1.class.to_s]
+        ref_id1 = ref1.id
+
+        citacao1 = citacao(title1, citacao_type1, class_type1, ref_id1, "[[#{Conversor::REFERENCES_TYPE.invert[ref1.class.to_s]}#{ref1.id} #{ref1.title}]]")
+
+        prefix = Faker::Lorem.paragraph(1)
+        sufix = Faker::Lorem.paragraph(1)
+
+        text = prefix+citacao+sufix+citacao1
+
+        text = Conversor::convert_text(text, tcc)
+        text.rstrip.should == prefix+citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)+sufix+citacao(title1, citacao_type1, class_type1, ref_id1, ref1.direct_citation)
+
+      end
     end
     describe 'general_refs' do
+      it 'should convert citacao direta' do
+        ref = Fabricate(:general_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
 
+        title = ref.reference_text
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.reference_text}]]")
+        text = Conversor::convert_text(citacao, tcc)
+
+        text.rstrip.should == citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)
+
+      end
+      it 'should convert citacao indireta' do
+        ref = Fabricate(:general_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
+
+        title = ref.reference_text
+        citacao_type = "ci"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.reference_text}]]")
+        text = Conversor::convert_text(citacao, tcc)
+
+        text.rstrip.should == citacao(title, citacao_type, class_type, ref_id, ref.indirect_citation)
+      end
+      it 'should just convert citacao' do
+        ref = Fabricate(:general_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
+
+        title = ref.reference_text
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+
+        prefix = Faker::Lorem.paragraph(1)
+        sufix = Faker::Lorem.paragraph(1)
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.reference_text}]]")
+        text = prefix+citacao+sufix
+
+        text = Conversor::convert_text(text, tcc)
+        text.rstrip.should == prefix+citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)+sufix
+
+      end
+
+      it 'should convert all citacao from text' do
+        ref = Fabricate(:general_ref)
+        ref1 = Fabricate(:general_ref)
+
+        tcc.save!
+        tcc.references.create!(element: ref)
+        tcc.references.create!(element: ref1)
+
+        title = ref.reference_text
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.reference_text}]]")
+
+        title1 = ref1.reference_text
+        citacao_type1 = "cd"
+        class_type1 = Conversor::REFERENCES_TYPE.invert[ref1.class.to_s]
+        ref_id1 = ref1.id
+
+        citacao1 = citacao(title1, citacao_type1, class_type1, ref_id1, "[[#{Conversor::REFERENCES_TYPE.invert[ref1.class.to_s]}#{ref1.id} #{ref1.reference_text}]]")
+
+        prefix = Faker::Lorem.paragraph(1)
+        sufix = Faker::Lorem.paragraph(1)
+
+        text = prefix+citacao+sufix+citacao1
+
+        text = Conversor::convert_text(text, tcc)
+        text.rstrip.should == prefix+citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)+sufix+citacao(title1, citacao_type1, class_type1, ref_id1, ref1.direct_citation)
+
+      end
     end
     describe 'internet_ref' do
       it 'should convert citacao direta' do
@@ -104,7 +436,90 @@ describe Conversor do
       end
     end
     describe 'legislative_refs' do
+      it 'should convert citacao direta' do
+        ref = Fabricate(:legislative_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
 
+        title = ref.title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.title}]]")
+        text = Conversor::convert_text(citacao, tcc)
+
+        text.rstrip.should == citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)
+
+      end
+      it 'should convert citacao indireta' do
+        ref = Fabricate(:legislative_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
+
+        title = ref.title
+        citacao_type = "ci"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.title}]]")
+        text = Conversor::convert_text(citacao, tcc)
+
+        text.rstrip.should == citacao(title, citacao_type, class_type, ref_id, ref.indirect_citation)
+      end
+      it 'should just convert citacao' do
+        ref = Fabricate(:legislative_ref)
+        tcc.save!
+        tcc.references.create!(element: ref)
+
+        title = ref.title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+
+        prefix = Faker::Lorem.paragraph(1)
+        sufix = Faker::Lorem.paragraph(1)
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.title}]]")
+        text = prefix+citacao+sufix
+
+        text = Conversor::convert_text(text, tcc)
+        text.rstrip.should == prefix+citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)+sufix
+
+      end
+
+      it 'should convert all citacao from text' do
+        ref = Fabricate(:legislative_ref)
+        ref1 = Fabricate(:legislative_ref)
+
+        tcc.save!
+        tcc.references.create!(element: ref)
+        tcc.references.create!(element: ref1)
+
+        title = ref.title
+        citacao_type = "cd"
+        class_type = Conversor::REFERENCES_TYPE.invert[ref.class.to_s]
+        ref_id = ref.id
+
+        citacao = citacao(title, citacao_type, class_type, ref_id, "[[#{Conversor::REFERENCES_TYPE.invert[ref.class.to_s]}#{ref.id} #{ref.title}]]")
+
+        title1 = ref1.title
+        citacao_type1 = "cd"
+        class_type1 = Conversor::REFERENCES_TYPE.invert[ref1.class.to_s]
+        ref_id1 = ref1.id
+
+        citacao1 = citacao(title1, citacao_type1, class_type1, ref_id1, "[[#{Conversor::REFERENCES_TYPE.invert[ref1.class.to_s]}#{ref1.id} #{ref1.title}]]")
+
+        prefix = Faker::Lorem.paragraph(1)
+        sufix = Faker::Lorem.paragraph(1)
+
+        text = prefix+citacao+sufix+citacao1
+
+        text = Conversor::convert_text(text, tcc)
+        text.rstrip.should == prefix+citacao(title, citacao_type, class_type, ref_id, ref.direct_citation)+sufix+citacao(title1, citacao_type1, class_type1, ref_id1, ref1.direct_citation)
+
+      end
     end
 
 
