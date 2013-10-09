fe3e8ca3e9046418da1c239e8d01b04d5a0102c3
Gabriel Mazetto <brodock@gmail.com>
2013-10-09 17:59:14 -0300

    Acrescentado configuração de e-mail de notificação

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,12 +1,13 @@
+# encoding: utf-8
 class Mailer < ActionMailer::Base
-  default from: "example@email.com"
+  default from: Proc.new { TCC_CONFIG['notification_email'] }
 
   def state_altered(email, old_state, new_state, url)
     @old = old_state
     @new = new_state
     @url = url
 
-    mail(:to => email, :subject => '[UNA-SUS TCC] Estado alterado')
+    mail(:to => email, :subject => '[UNA-SUS TCC] novas alterações no TCC')
   end
 
 
