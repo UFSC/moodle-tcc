2956d0ff20e571775b37e3d77c5c340405431530
Gabriel Mazetto <brodock@gmail.com>
2013-10-15 12:03:19 -0300

    Correções na troca de estados

--- app/models/abstract.rb
+++ app/models/abstract.rb
@@ -1,15 +1,20 @@
 class Abstract < ActiveRecord::Base
+  extend Enumerize
   include TccStateMachine
-
   belongs_to :tcc
 
-  attr_accessible :commentary, :content, :key_words
+  attr_accessible :commentary, :content, :key_words, :new_state
 
   validates :content, presence: true, unless: Proc.new { |abstract| abstract.new? }
 
+  enumerize :new_state, in: aasm_states
   has_paper_trail meta: {state: :state}
 
   def empty?
     self.content.blank? && self.key_words.blank?
+  end
+
+  def self.new_states_collection
+    new_state.options - [['Finalizado', 'terminated']] - [['Novo', 'new']]
   end
 end
--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -1,15 +1,20 @@
 class Presentation < ActiveRecord::Base
+  extend Enumerize
   include TccStateMachine
   belongs_to :tcc
 
-  attr_accessible :commentary, :content
+  attr_accessible :commentary, :content, :new_state
 
   validates :content, presence: true, unless: Proc.new { |presentation| presentation.new? }
 
+  enumerize :new_state, in: aasm_states
   has_paper_trail meta: {state: :state}
 
   def empty?
     self.content.blank?
   end
 
+  def self.new_states_collection
+    new_state.options - [['Finalizado', 'terminated']] - [['Novo', 'new']]
+  end
 end
