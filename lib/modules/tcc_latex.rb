ce3aa77b759238e9cb0f12d19a2a360f454a5c2f
Osmar <osmar@setic.ufsc.br>
2013-10-18 12:18:00 -0300

    Estrutura do pdf em formato abtn

--- lib/modules/tcc_latex.rb
+++ lib/modules/tcc_latex.rb
@@ -1,6 +1,27 @@
 module TccLatex
   unloadable
 
+  def self.apply_latex(text)
+    # Substituir caracteres html pelo respectivo em utf-8
+    coder = HTMLEntities.new
+    content = coder.decode(text)
+    html = Nokogiri::HTML(content)
+
+    # XHTML bem formado
+    doc = Nokogiri::XML(html.to_xhtml)
+
+    # Aplicar xslt
+    xh2file = Rails.public_path + '/xh2latex.xsl'
+    xslt  = Nokogiri::XSLT(File.read(xh2file))
+    transform = xslt.apply_to(doc)
+
+    # Remover begin document, pois ja está no layout
+    tex = transform.gsub('\begin{document}','').gsub('end{document}','')
+    return tex
+  end
+
+
+
   def self.references
     #criar html do references
     htmlfile = Rails.public_path + '/references.html'
