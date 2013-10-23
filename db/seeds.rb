13e902f02c4279245fd5d24dc459b9e512060a0c
fabioariati <fabiocariati@gmail.com>
2013-10-23 18:22:32 -0200

    Acrescentado um título extendido nas definições do HUB
    Este título extendido, é utilizado na impressão dos TCCs
    e na pré-visualização

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,10 @@ def create_or_update_tcc_definitions(title, hub_diaries, activity_url)
     Progress.step
 
     hub_diaries.each_with_index do |(hub_name, diaries), hub_index|
-      hub_def = HubDefinition.find_or_create_by_position_and_tcc_definition_id(position: hub_index+1, title: hub_name, tcc_definition_id: tcc_def.id, moodle_shortname: diaries[0][:shortname])
+      hub_def = HubDefinition.find_or_initialize_by_position_and_tcc_definition_id( position: hub_index+1, tcc_definition_id: tcc_def.id)
+      hub_def.moodle_shortname = diaries[0][:shortname]
+      hub_def.subtitle = hub_name[1]
+      hub_def.save
       Progress.step
 
       diaries.each_with_index do |diary, diary_index|
@@ -35,15 +38,15 @@ end
 # TCC Definition - Turma A
 #
 definition = {
-    'Eixo 1' => [{1264 => 'Saúde e sociedade'}, {1092 => 'Epidemiologia'}],
-    'Eixo 2' => [{1151 => 'Planejamento na Atenção Básica'}, {1133 => 'Gestão e Avaliação na Atenção Básica'}, {1163 => 'Processo de Trabalho na Atenção Básica'}],
-    'Eixo 3 - ESF' => [
+    ['Eixo 1', 'Reconhecimento da Realidade'] => [{1264 => 'Saúde e sociedade'}, {1092 => 'Epidemiologia'}],
+    ['Eixo 2', 'O trabalho da atenção Básica'] => [{1151 => 'Planejamento na Atenção Básica'}, {1133 => 'Gestão e Avaliação na Atenção Básica'}, {1163 => 'Processo de Trabalho na Atenção Básica'}],
+    ['Eixo 3 - ESF', 'A assistência na Atenção Básica'] => [
         {1940 => 'Atenção Integral à Saúde da Criança', shortname: 'SPB110069-21000077ES (20131)'},
         {2055 => 'Atenção Integral à Saúde da Mulher'}, # shortname: 'SPB110070-21000077ES (20131)'
         {2637 => 'Atenção Integral à Saúde do Adulto'}, # shortname: 'SPB110071-21000077ES (20131)'
         {2693 => 'Atenção Integral à Saúde do Idoso'} # shortname: 'SPB110072-21000077ES (20131)'
     ],
-    'Eixo 3 - NASF' => [
+    ['Eixo 3 - NASF', 'A assistência na Atenção Básica'] => [
         {2258 => 'Apoio Matricial', shortname: 'SPB110073-21000077ES (20131)'},
         {1994 => 'Clínica Ampliada'}, # shortname: 'SPB110074-21000077ES (20131)'
         {2014 => 'Projeto Terapêutico Singular'}, # shortname: 'SPB110075-21000077ES (20131)'
@@ -58,15 +61,15 @@ create_or_update_tcc_definitions('Turma A', definition, activity_url)
 # TCC Definition - Turma B
 #
 definition = {
-    'Eixo 1' => [{1550 => 'Saúde e sociedade'}, {1569 => 'Epidemiologia'}],
-    'Eixo 2' => [{1786 => 'Planejamento na Atenção Básica'}, {1803 => 'Gestão e Avaliação na Atenção Básica'}, {2431 => 'Processo de Trabalho na Atenção Básica'}],
-    'Eixo 3 - ESF' => [
+    ['Eixo 1', 'Reconhecimento da Realidade'] => [{1550 => 'Saúde e sociedade'}, {1569 => 'Epidemiologia'}],
+    ['Eixo 2', 'O trabalho da atenção Básica'] => [{1786 => 'Planejamento na Atenção Básica'}, {1803 => 'Gestão e Avaliação na Atenção Básica'}, {2431 => 'Processo de Trabalho na Atenção Básica'}],
+    ['Eixo 3 - ESF', 'A assistência na Atenção Básica'] => [
         {2327 => 'Atenção Integral à Saúde da Criança', shortname: 'SPB110069-21000077ES (20132)'}, # shortname: 'SPB110069-21000077ES (20132)'
         {2388 => 'Atenção Integral à Saúde da Mulher'}, # shortname: 'SPB110070-21000077ES (20132)'
         {2078 => 'Atenção Integral à Saúde do Adulto'}, # shortname: 'SPB110071-21000077ES (20132)'
         {2124 => 'Atenção Integral à Saúde do Idoso'} # shortname: 'SPB110072-21000077ES (20132)'
     ],
-    'Eixo 3 - NASF' => [
+    ['Eixo 3 - NASF', 'A assistência na Atenção Básica'] => [
         {2322 => 'Apoio Matricial', shortname: 'SPB110073-21000077ES (20132)'}, # shortname: 'SPB110073-21000077ES (20132)'
         {2599 => 'Clínica Ampliada'}, # shortname: 'SPB110074-21000077ES (20132)'
         {2661 => 'Projeto Terapêutico Singular'}, # shortname: 'SPB110075-21000077ES (20132)'
