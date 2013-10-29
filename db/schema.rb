03049d6618ef11df743c19e05b8b8bd2364b07f0
Gabriel Mazetto <brodock@gmail.com>
2013-10-29 16:29:31 -0200

    Correção no dbschema
    Em algum branch anterior foi adicionado um course_id em uma tabela
    que não faz mais sentido, e o arquivo não foi atualizado.

--- db/schema.rb
+++ db/schema.rb
@@ -189,7 +189,6 @@ ActiveRecord::Schema.define(:version => 20131002214201) do
     t.string   "title"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
-    t.string   "course_id"
     t.string   "activity_url"
   end
 
