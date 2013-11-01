199c71558a75d57eceacc534fb860e0d6cbd6f93
Gabriel Mazetto <brodock@gmail.com>
2013-11-01 17:19:25 -0200

    Alterado script de deploy pra utilizar o branch atual no staging
    Com essa alteração, o deploy pro staging passa a ser do branch atual.
    Isso facilita a integração dos tickets, pois permite enviar pro staging
    um ticket que ainda não foi feito merge com o master.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,1 +1,9 @@
+# Figure out the name of the current local branch
+def current_git_branch
+  branch = `git symbolic-ref HEAD 2> /dev/null`.strip.gsub(/^refs\/heads\//, '')
+  puts "Deploying branch #{branch}"
+  branch
+end
+
 set :deploy_to, '/home/gabriel/tcc.labsoft.ufsc.br'
+set :branch, current_git_branch
