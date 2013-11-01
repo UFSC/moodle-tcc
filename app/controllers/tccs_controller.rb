8dde4a1e0ece2441d1c6a3c2579a874bb80cda0f
Gabriel Mazetto <brodock@gmail.com>
2013-11-01 16:26:06 -0200

    Pré-visualização do TCC inclui agora todos os dados relevantes
    * Inclui informações corretas do orientador
    * Inclui informações corretas do Estudante
    * Inclui o conteúdo dos diários
    * Inclui inclui as informações do Resumo
    * Inclui as informações da apresentação
    * Localiza corretamente as reflexões dos tccs e título da reflexão
    
    Foi acrescentado uma caixa na parte superior do documento para avisar
    que esta é uma pré-visualização "Rascunho" com finalidade de leitura.

--- app/controllers/tccs_controller.rb
+++ app/controllers/tccs_controller.rb
@@ -37,8 +37,13 @@ class TccsController < ApplicationController
 
   def preview_tcc
     @current_user = current_user
+    @matricula = MoodleUser::find_username_by_user_id(@tcc.moodle_user)
+    @nome_orientador = Middleware::Orientadores.find_by_cpf(@tcc.orientador).try(:nome) if @tcc.orientador
 
+    @abstract = @tcc.abstract
+    @presentation = @tcc.presentation
     @hubs = @tcc.hubs.hub_tcc
+    @hubs.each { |hub| hub.fetch_diaries(@user_id) }
     @final_considerations = @tcc.final_considerations
   end
 
