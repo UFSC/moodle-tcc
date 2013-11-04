ba3e18bc52a189f1871af52a15671a574b78790e
fabioariati <fabiocariati@gmail.com>
2013-11-04 15:41:33 -0200

    Retirado códigos desnecessários gerados e atualizado o gitgnore

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -28,16 +28,16 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
   
-  process :read_dimensions
+  #process :read_dimensions
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fill => [118, 100]
-  end
-
-  version :content do
-    process :resize_to_limit => [800, 800]
-  end
+  #version :thumb do
+  #  process :resize_to_fill => [118, 100]
+  #end
+  #
+  #version :content do
+    #process :resize_to_limit => [800, 800]
+  #end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
