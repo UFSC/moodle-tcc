bb18886cb060cb9bda4cf499b924acf770a094f3
fabioariati <fabiocariati@gmail.com>
2013-11-06 17:13:24 -0200

    Spec faltante para o uploader do ckeditor

---
+++ spec/uploaders/ckeditor_picture_uploader_spec.rb
@@ -0,0 +1,27 @@
+require 'carrierwave/test/matchers'
+
+describe CkeditorPictureUploader do
+  include CarrierWave::Test::Matchers
+
+  before do
+    CkeditorPictureUploader.enable_processing = true
+    @uploader = CkeditorPictureUploader.new(Ckeditor::Picture.new, :data)
+    @uploader.store!(File.open("#{Rails.root}/spec/support/image_test.jpg"))
+  end
+
+  after do
+    CkeditorPictureUploader.enable_processing = false
+    @uploader.remove!
+  end
+
+  context 'the a4 version' do
+    it "should scale down a landscape image to fit within a4 pattern" do
+      width = 500
+      @uploader.a4.should be_no_larger_than(width, (Math.sqrt(2)*width).to_i)
+    end
+  end
+
+  it "should make the file.." do
+    @uploader.should have_permissions(0644)
+  end
+end
