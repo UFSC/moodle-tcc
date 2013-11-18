8c33001cb3def15ba57793fd55912a1385a1c3dd
Gabriel Mazetto <brodock@gmail.com>
2013-11-18 18:57:13 -0200

    Correções de inicialização do LatexToPdf para produção

--- config/initializers/latex.rb
+++ config/initializers/latex.rb
@@ -1,2 +1,12 @@
+# encoding: utf-8
+
+# Descobre o diretório do texlive no linux e mac
+texlive_bin_dir = Dir.glob('/usr/local/texlive/2013/bin/x86_64-*').first
+if texlive_bin_dir.empty?
+  $stderr.puts 'Texlive não está instalado corretamente'
+  exit 1
+end
+
+ENV['PATH']="#{texlive_bin_dir}:#{ENV['PATH']}"
 LatexToPdf.config[:arguments].delete('-halt-on-error')
 LatexToPdf.config.merge! parse_twice: true, bibtex: true
