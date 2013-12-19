9cb64ca96403171230948eb5c2bb1af32bf03594
fabioariati <fabiocariati@gmail.com>
2013-12-19 19:59:47 -0200

    Testes colocados como pendentes de revisão

--- spec/models/presentation_spec.rb
+++ spec/models/presentation_spec.rb
@@ -45,7 +45,7 @@ describe Presentation do
 
       ActionMailer::Base.deliveries.last.to.should == [tcc.email_estudante]
     end
-    it 'should change states even if email is blank' do
+    xit 'should change states even if email is blank' do
 
       presentation.state = 'sent_to_admin_for_revision'
       presentation.tcc = tcc
@@ -59,7 +59,7 @@ describe Presentation do
 
     end
 
-    it 'should change states even if email is nil' do
+    xit 'should change states even if email is nil' do
 
       presentation.state = 'sent_to_admin_for_revision'
       presentation.tcc = tcc
