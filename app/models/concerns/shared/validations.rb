5cd9eaa8a73f1263b66e04d81d20f46e53260b2d
fabioariati <fabiocariati@gmail.com>
2014-01-27 17:34:10 -0200

    Validaçao para nome completo e retirada exceto internet_refs.

---
+++ app/models/concerns/shared/validations.rb
@@ -0,0 +1,13 @@
+module Shared::Validations
+  extend ActiveSupport::Concern
+
+  class CompletenameValidator < ActiveModel::EachValidator
+    def validate_each(record, attribute, value)
+      unless value.nil? || value.blank?
+        unless value.split(' ').size > 1
+          record.errors[attribute] << I18n.t('is_not_complete_name')
+        end
+      end
+    end
+  end
+end
