1d527311e178fc07d07d3292a79b0f0ca7c622fa
Bruno Silveira <bruno.silveira@setic.ufsc.br>
2014-01-29 10:56:38 -0200

    Correção do Fabricator para conter todos os autores e testes corrigidos.

--- spec/models/book_cap_ref_spec.rb
+++ spec/models/book_cap_ref_spec.rb
@@ -5,7 +5,7 @@ describe BookCapRef do
     before(:all) { @book_cap_ref = Fabricate(:book_cap_ref) }
     after(:all) { @book_cap_ref.destroy }
 
-    it { should respond_to(:book_author, :book_subtitle, :book_title, :cap_author, :cap_subtitle, :cap_title, :end_page, :initial_page, :local, :publisher, :type_participation, :year) }
+    it { should respond_to(:first_entire_author, :book_subtitle, :book_title, :first_part_author, :cap_subtitle, :cap_title, :end_page, :initial_page, :local, :publisher, :type_participation, :year) }
     it { should have_one(:reference) }
 
     # Pending
@@ -19,9 +19,9 @@ describe BookCapRef do
     it { should validate_presence_of(:year) }
     it { should validate_presence_of(:publisher) }
     it { should validate_presence_of(:cap_title) }
-    it { should validate_presence_of(:cap_author) }
+    it { should validate_presence_of(:first_part_author) }
     it { should validate_presence_of(:book_title) }
-    it { should validate_presence_of(:book_author) }
+    it { should validate_presence_of(:first_entire_author) }
     it { should validate_presence_of(:type_participation) }
     it { should validate_presence_of(:initial_page) }
     it { should validate_numericality_of(:initial_page) }
@@ -50,10 +50,18 @@ describe BookCapRef do
   end
 
   context 'normalizations' do
-    it { should normalize_attribute(:book_author) }
-    it { should normalize_attribute(:book_author).from(' Nome   Completo  ').to('Nome Completo') }
-    it { should normalize_attribute(:cap_author) }
-    it { should normalize_attribute(:cap_author).from(' Nome   Completo  ').to('Nome Completo') }
+    it { should normalize_attribute(:first_entire_author) }
+    it { should normalize_attribute(:first_entire_author).from(' Nome   Completo  ').to('Nome Completo') }
+    it { should normalize_attribute(:second_entire_author) }
+    it { should normalize_attribute(:second_entire_author).from(' Nome   Completo  ').to('Nome Completo') }
+    it { should normalize_attribute(:third_entire_author) }
+    it { should normalize_attribute(:third_entire_author).from(' Nome   Completo  ').to('Nome Completo') }
+    it { should normalize_attribute(:first_part_author) }
+    it { should normalize_attribute(:first_part_author).from(' Nome   Completo  ').to('Nome Completo') }
+    it { should normalize_attribute(:second_part_author) }
+    it { should normalize_attribute(:second_part_author).from(' Nome   Completo  ').to('Nome Completo') }
+    it { should normalize_attribute(:third_part_author) }
+    it { should normalize_attribute(:third_part_author).from(' Nome   Completo  ').to('Nome Completo') }
     it { should normalize_attribute(:book_title) }
     it { should normalize_attribute(:book_title).from(' Nome   Completo  ').to('Nome Completo') }
     it { should normalize_attribute(:local) }
@@ -105,7 +113,7 @@ describe BookCapRef do
 
       it 'subtype should be nil' do
         book_cap_ref1 = Fabricate.build(:book_cap_ref)
-        book_cap_ref1.book_author = 'Autor A1'
+        book_cap_ref1.first_entire_author = 'Autor A1'
 
         book_cap_ref1.save!
 
@@ -114,7 +122,7 @@ describe BookCapRef do
 
       it 'subtype should be nil after one update' do
         book_cap_ref1 = Fabricate.build(:book_cap_ref)
-        book_cap_ref1.book_author = 'Autor A1'
+        book_cap_ref1.first_entire_author = 'Autor A1'
 
         book_cap_ref1.save!
         book_cap_ref1.save!
@@ -124,12 +132,16 @@ describe BookCapRef do
 
       it 'subtype should be set correctly' do
         book_cap_ref1 = Fabricate.build(:book_cap_ref)
-        book_cap_ref1.book_author = 'Autor A1'
+        book_cap_ref1.first_entire_author = 'Autor A1'
+        book_cap_ref1.second_entire_author = 'Autor A2'
+        book_cap_ref1.third_entire_author = 'Autor A3'
 
         book_cap_ref1.save!
 
         book_cap_ref2 = Fabricate.build(:book_cap_ref)
-        book_cap_ref2.book_author = 'Autor A1'
+        book_cap_ref2.first_entire_author = 'Autor A1'
+        book_cap_ref2.second_entire_author = 'Autor A2'
+        book_cap_ref2.third_entire_author = 'Autor A3'
 
         book_cap_ref2.save!
 
@@ -142,12 +154,16 @@ describe BookCapRef do
 
       it 'should set subtype to nil if object is different' do
         book_cap_ref1 = Fabricate.build(:book_cap_ref)
-        book_cap_ref1.book_author = 'Autor A1'
+        book_cap_ref1.first_entire_author = 'Autor A1'
+        book_cap_ref1.second_entire_author = 'Autor A2'
+        book_cap_ref1.third_entire_author = 'Autor A3'
 
         book_cap_ref1.save!
 
         book_cap_ref2 = Fabricate.build(:book_cap_ref)
-        book_cap_ref2.book_author = 'Autor A1'
+        book_cap_ref2.first_entire_author = 'Autor A1'
+        book_cap_ref2.second_entire_author = 'Autor A2'
+        book_cap_ref2.third_entire_author = 'Autor A3'
 
         book_cap_ref2.save!
 
@@ -157,7 +173,7 @@ describe BookCapRef do
         book_cap_ref2.subtype.should == 'b'
 
         book_cap_ref2
-        book_cap_ref2.book_author = 'Autor A10'
+        book_cap_ref2.first_entire_author = 'Autor A10'
         book_cap_ref2.save!
         book_cap_ref1.reload
         book_cap_ref2.reload
@@ -170,7 +186,7 @@ describe BookCapRef do
     end
   end
   context '#indirect_citation' do
-    it_should_behave_like "indirect_citation" do
+    it_should_behave_like "indirect_citation with more than one author" do
       let(:ref) { Fabricate(:book_cap_ref) }
     end
   end
