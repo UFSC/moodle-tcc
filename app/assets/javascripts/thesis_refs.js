3c4c6b55aec2675a5168f09144db66341c2c9d29
Bruno Silveira <bruno.silveira@setic.ufsc.br>
2014-02-12 14:36:08 -0200

    Otimização do Javascript

--- app/assets/javascripts/thesis_refs.js
+++ app/assets/javascripts/thesis_refs.js
@@ -1,37 +1,9 @@
 $(function() {
     $('input[name="thesis_ref[type_thesis]"]').on('change', function() {
-
-        switch ($(this).val()) {
-            case 'Tese':
-                $("#thesis_ref_degree option:contains(Doutorado)").attr('selected', true);
-                $("#thesis_ref_degree option:contains(Mestrado)").attr('selected', false);
-                $("#thesis_ref_degree option:contains(Especialização)").attr('selected', false);
-                $("#thesis_ref_degree option:contains(Graduação)").attr('selected', false);
-                $('#degree').val('Doutorado');
-                break;
-            case 'Dissertação':
-                $("#thesis_ref_degree option:contains(Mestrado)").attr('selected', true);
-                $("#thesis_ref_degree option:contains(Doutorado)").attr('selected', false);
-                $("#thesis_ref_degree option:contains(Especialização)").attr('selected', false);
-                $("#thesis_ref_degree option:contains(Graduação)").attr('selected', false);
-                $('#degree').val('Mestrado');
-                break;
-            case 'Monografia':
-                $("#thesis_ref_degree option:contains(Especialização)").attr('selected', true);
-                $("#thesis_ref_degree option:contains(Doutorado)").attr('selected', false);
-                $("#thesis_ref_degree option:contains(Mestrado)").attr('selected', false);
-                $("#thesis_ref_degree option:contains(Graduação)").attr('selected', false);
-                $('#degree').val('Especialização');
-                break;
-            case 'TCC':
-                $("#thesis_ref_degree option:contains(Graduação)").attr('selected', true);
-                $("#thesis_ref_degree option:contains(Doutorado)").attr('selected', false);
-                $("#thesis_ref_degree option:contains(Mestrado)").attr('selected', false);
-                $("#thesis_ref_degree option:contains(Especialização)").attr('selected', false);
-                $('#degree').val('Graduação');
-                break;
-            default:
-                return false;
-        }
+        $("#thesis_ref_degree option:contains(Doutorado)").attr('selected', $(this).val() == 'Tese');
+        $("#thesis_ref_degree option:contains(Mestrado)").attr('selected', $(this).val() == 'Dissertação');
+        $("#thesis_ref_degree option:contains(Especialização)").attr('selected', $(this).val() == 'Monografia');
+        $("#thesis_ref_degree option:contains(Graduação)").attr('selected', $(this).val() == 'TCC');
+        $('#degree').val($("#thesis_ref_degree").val());
     });
 });
