9ee1fee6ef9ee3e7f45abd35c1fe490aa97b0f93
Gabriel Mazetto <brodock@gmail.com>
2014-08-15 16:47:18 -0300

    Correção do spec_helper para RSpec 3.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'rake' # Workaround for Fabrication gem bug
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rspec'
-require "rack_session_access/capybara"
+require 'rack_session_access/capybara'
 
 
 # Requires supporting ruby files with custom matchers and macros, etc,
