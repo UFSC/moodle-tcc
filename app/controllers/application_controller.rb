c9244e8619e6594441402920e04fce0a7f09d16a
Gabriel Mazetto <brodock@gmail.com>
2014-08-21 18:09:26 -0300

    Adicionado header X-Frame-Options para garantir exibição em iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   include Authentication
   include LtiTccFilters
   protect_from_forgery with: :exception
+  before_action :allow_iframe
 
   # Set current_user as assetable
   def ckeditor_before_create_asset(asset)
     asset.assetable = @tcc
     return true
+  end
+
+  def allow_iframe
+    response.headers['X-Frame-Options'] = "ALLOW-FROM #{Settings.moodle_url}"
   end
 end
