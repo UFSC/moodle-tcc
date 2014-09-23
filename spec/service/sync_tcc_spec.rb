065eb909cdceb2832b36d3e54872de27fcdf03eb
luizhsalazar@gmail.com <luizhsalazar@gmail.com>
2014-09-23 16:13:33 -0300

    Ajustes testes da sincronização
    Concluído os testes para a criação de pessoas (tutor, orientador e
    estudante) e início dos testes para o contexto do TCC

--- spec/service/sync_tcc_spec.rb
+++ spec/service/sync_tcc_spec.rb
@@ -10,34 +10,51 @@ describe SyncTcc do
     let(:tutor) { Fabricate.build(:person) }
     let(:orientador) { Fabricate.build(:person) }
 
-    it 'expects tutor to be created'
-
-    it 'expects orientador to be created'
-
-    it 'expects student to be created' do
-      allow(MoodleAPI::MoodleUser).to receive(:get_students_by_course) { student.moodle_id }
+    it 'expects tutor to be created' do
+      allow(MoodleAPI::MoodleUser).to receive(:get_students_by_course) { [tutor.moodle_id] }
       allow(sync).to receive(:get_tutor) { tutor }
       allow(sync).to receive(:get_orientador) { orientador }
-
       sync.call
-
-      expect(Person.find_by_moodle_id student.moodle_id).to be_a(Person)
+      expect(Person.find_by_moodle_id tutor.moodle_id).to be_a(Person)
     end
 
+    it 'expects orientador to be created' do
+      allow(MoodleAPI::MoodleUser).to receive(:get_students_by_course) { [orientador.moodle_id] }
+      allow(sync).to receive(:get_tutor) { tutor }
+      allow(sync).to receive(:get_orientador) { orientador }
+      sync.call
+      expect(Person.find_by_moodle_id orientador.moodle_id).to be_a(Person)
+    end
 
-
+    it 'expects student to be created' do
+      allow(MoodleAPI::MoodleUser).to receive(:get_students_by_course) { [student.moodle_id] }
+      allow(sync).to receive(:get_tutor) { tutor }
+      allow(sync).to receive(:get_orientador) { orientador }
+      sync.call
+      expect(Person.find_by_moodle_id student.moodle_id).to be_a(Person)
+    end
   end
 
   context 'TCC' do
 
+    let(:tcc_definition) { Fabricate(:tcc_definition) }
+    let(:sync) { SyncTcc.new(tcc_definition) }
+    let(:tcc) { Fabricate.build(:tcc) }
+    let(:_student) { Fabricate.build(:person) }
+
     it 'expects to be created' do
-      allow(SyncTcc).to receive(:get_students) { [person] }
+      allow(MoodleAPI::MoodleUser).to receive(:get_students_by_course) { [_student.moodle_id] }
+      allow(sync).to receive(:get_tutor) { _student }
+      allow(sync).to receive(:get_orientador) { _student }
+      sync.call
+      expect(Tcc.find_by_tcc_definition_id tcc_definition).to be_a(Tcc)
     end
 
-    it 'expects to be updated'
+    it 'expects to be updated' do
+    end
 
-    it 'expects to not duplicate when updated'
-
+    it 'expects to not duplicate when updated' do
+    end
   end
 
 end
