b1028859c63c8bfd8d98fe27599e7798529fc1f6
Gabriel Mazetto <brodock@gmail.com>
2014-10-01 17:14:06 -0300

    Remoção de códigos que não são mais utilizados

--- lib/modules/middleware_user.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-module MiddlewareUser
-  def self.check_enrol(username, shortname)
-    # TODO: tratar falhas de conexão
-    matricula = MoodleAPI::MoodleUser::find_username_by_user_id(username)
-
-    Middleware::InscricoesCurso.where(shortname: shortname).where(username: matricula).any?
-  end
-end
