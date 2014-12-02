4512e38e2eef7e7b426bc2364b04ecc8c05f68cf
Gabriel Mazetto <brodock@gmail.com>
2014-12-02 20:12:52 -0200

    Refatoração dos nomes compostos (AJAX)

--- app/controllers/compound_names_controller.rb
+++ app/controllers/compound_names_controller.rb
@@ -5,9 +5,30 @@ class CompoundNamesController < ApplicationController
 
   before_filter :set_current_tab
 
+  def new
+    authorize(@tcc, :show_compound_names?)
+
+    @modal_title = t(:add_compound_name)
+    @compound_name = CompoundName.new(type_name: 'simple')
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def edit
+    @modal_title = t(:edit_compound_name)
+    @compound_name = CompoundName.find(params[:id])
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def create
     authorize(@tcc, :show_compound_names?)
     compound_name = CompoundName.new(params[:compound_name])
+
     if compound_name.save
       flash[:success] = t(:successfully_saved)
     end
@@ -18,9 +39,11 @@ class CompoundNamesController < ApplicationController
   def update
     authorize(@tcc, :show_compound_names?)
     compound_name = CompoundName.find(params[:id])
+
     if compound_name.update_attributes(params[:compound_name])
       flash[:success] = t(:successfully_saved)
     end
+
     redirect_to bibliographies_path(moodle_user: params[:moodle_user], anchor: 'compound_names')
   end
 
