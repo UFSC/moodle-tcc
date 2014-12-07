3ffcfee40643d5047a1a2f78e4460d7304726d42
Gabriel Mazetto <brodock@gmail.com>
2014-12-07 16:57:00 -0200

    Revert "Removendo action não utilizada"
    This reverts commit e8e6f5e8bb63330b51daf4bfef6da2eaf4f473b0.

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -45,6 +45,13 @@ class ChaptersController < ApplicationController
     render :edit
   end
 
+  def empty
+    set_tab ('chapter'+params[:position]).to_sym
+    @chapter = @tcc.chapters.find_by(position: params[:position])
+
+    authorize @chapter
+  end
+
   def import
     set_tab ('chapter'+params[:position]).to_sym
     @chapter = @tcc.chapters.find_by(position: params[:position])
