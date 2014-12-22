6c5cb2bb483d416a8534b6e27d7f7173d4b96b4e
Roberto Cunha <robertosilvino@gmail.com>
2014-12-22 14:58:57 -0200

    ajuste na atualização da tela por causa do Windows

--- app/assets/javascripts/instructor_admin.js
+++ app/assets/javascripts/instructor_admin.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
     if($('#refresh-list').length){
+        //$('#search').val($('#search').val()+' refresh');
+        $('#tcc-focus').val('true');
         window.onfocus = function() {
-            location.reload();
+            // essa condicional foi colocada por causa do Windows (SO)
+            // Entra duas vezes no focus antes do blur quando pressiona dropdown
+            if ($('#tcc-focus').val() != 'true') {
+                $('#tcc-focus').val('true');
+                //$('#search').val($('#search').val()+' refresh-focus');
+                location.reload();
+            }
+        };
+        window.onblur = function() {
+            //$('#search').val($('#search').val()+' refresh-blur');
+            $('#tcc-focus').val('false');
+
         };
     }
 });
