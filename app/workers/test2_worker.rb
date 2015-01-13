a2d701411450cd2fe899bec25b6b3cd08130c576
Roberto Silvino <robertosilvino@gmail.com>
2015-01-13 21:38:19 -0200

    refatoração da geração do pdf e implementação da URL temporaria

--- app/workers/test2_worker.rb
+++ app/workers/test2_worker.rb
@@ -2,69 +2,25 @@ class Test2Worker
   include Sidekiq::Worker
   sidekiq_options :queue => :teste, :retry => false
 
-  def generete_references (actionview, tcc)
-    content = actionview.render :file => "#{ Rails.root }/app/views/tccs/_bibtex.erb",
-                        :layout => false,
-                        locals: { book_refs: tcc.book_refs.decorate,
-                                  book_cap_refs: tcc.book_cap_refs.decorate,
-                                  article_refs: tcc.article_refs.decorate,
-                                  internet_refs: tcc.internet_refs.decorate,
-                                  legislative_refs: tcc.legislative_refs.decorate,
-                                  thesis_refs: tcc.thesis_refs.decorate
-                        }
-    @bibtex = TccDocument::ReferencesProcessor.new.execute(content)
-  end
-
   def filename(filename, extension)
     "tmp/#{filename}.#{extension}"
   end
 
   def perform(moodle_id)
     begin
-      student = Person.find_by_moodle_id(moodle_id)
-      @tcc = Tcc.find_by_student_id (student.id)
-      logger.info "Starting the TCC print: #{@tcc.id}"
-      puts("Start #{@tcc.student.name}")
+      pdf_service = PDFService.new(moodle_id)
+      puts("Starting the TCC print: #{moodle_id} - #{pdf_service.tcc.student.name}")
 
-      # create an instance of ActionView, so we can use the render method outside of a controller
-      av = ActionView::Base.new()
-      av.view_paths = ActionController::Base.view_paths
+      # pdfTex = pdf_service.generate_tex
+      # File.open(filename(pdf_service.tcc.student.name,'tex'), 'w+') do |f|
+      #   f.write(pdfTex)
+      # end
 
-      # need these in case your view constructs any links or references any helper methods.
-      av.class_eval do
-        include Rails.application.routes.url_helpers
-        include ApplicationHelper
+      pdf = pdf_service.generate_pdf
+      File.open(filename(pdf_service.tcc.student.name, 'pdf'), 'w') do |f|
+        f.write(pdf)
       end
-      # Tcc
-      tcc_document = LatexTccDecorator.new(@tcc)
-
-      # Resumo
-      abstract = LatexAbstractDecorator.new(@tcc.abstract)
-
-      # Capítulos
-      chapters = LatexChapterDecorator.decorate_collection(@tcc.chapters)
-
-      # Referencias
-      bibtex = generete_references(av, @tcc)
-
-      pdfTex = av.render pdf: "TCC ##{ @tcc.id }",
-                      file: "#{ Rails.root }/app/views/tccs/generate.pdf.erb",
-                      locals: { tcc_document: tcc_document,
-                                abstract: abstract,
-                                chapters: chapters,
-                                bibtex: bibtex
-                      }
-
-      File.open(filename(@tcc.student.name,'tex'), 'w+') do |f|
-        f.write(pdfTex)
-      end
-
-      @latex_config={:command => 'latex',:parse_twice => true}
-      result = LatexToPdf.generate_pdf(pdfTex, @latex_config)
-      File.open(filename(@tcc.student.name, 'pdf'), "w") do |f|
-        f.write(result)
-      end
-      puts("Ending the TCC print: #{@tcc.student.name}")
+      puts("Ending the TCC print: #{moodle_id} - #{pdf_service.tcc.student.name}")
     end
   end
 end
---
+++ lib/modules/pdf_service.rb
@@ -0,0 +1,77 @@
+class PDFService
+
+  def initialize(moodle_id)
+    student = Person.find_by_moodle_id(moodle_id)
+    @tcc = Tcc.find_by_student_id (student.id)
+  end
+
+  def tcc
+    @tcc
+  end
+
+  def generate_tex
+    # create an instance of ActionView, so we can use the render method outside of a controller
+    av = ActionView::Base.new()
+    av.view_paths = ActionController::Base.view_paths
+
+    # need these in case your view constructs any links or references any helper methods.
+    av.class_eval do
+      include Rails.application.routes.url_helpers
+      include ApplicationHelper
+    end
+    # Tcc
+    tcc_document = LatexTccDecorator.new(@tcc)
+
+    # Resumo
+    abstract = LatexAbstractDecorator.new(@tcc.abstract)
+
+    # Capítulos
+    chapters = LatexChapterDecorator.decorate_collection(@tcc.chapters)
+
+    # Referencias
+    bibtex = generete_references(av, @tcc)
+
+    pdfTex = av.render pdf: "TCC ##{ @tcc.id }",
+                       file: "#{ Rails.root }/app/views/tccs/generate.pdf.erb",
+                       locals: { tcc_document: tcc_document,
+                                 abstract: abstract,
+                                 chapters: chapters,
+                                 bibtex: bibtex
+                       }
+
+    # File.open(filename(@tcc.student.name,'tex'), 'w+') do |f|
+    #   f.write(pdfTex)
+    # end
+
+    pdfTex
+  end
+
+  def generate_pdf
+    pdfTex = generate_tex
+    latex_config={:command => 'latex',:parse_twice => true}
+    pdf = LatexToPdf.generate_pdf(pdfTex, latex_config)
+
+    # File.open(filename(@tcc.student.name, 'pdf'), "w") do |f|
+    #   f.write(result)
+    # end
+
+    pdf
+  end
+
+  private
+
+  def generete_references (actionview, tcc)
+    content = actionview.render :file => "#{ Rails.root }/app/views/tccs/_bibtex.erb",
+                                :layout => false,
+                                locals: { book_refs: tcc.book_refs.decorate,
+                                          book_cap_refs: tcc.book_cap_refs.decorate,
+                                          article_refs: tcc.article_refs.decorate,
+                                          internet_refs: tcc.internet_refs.decorate,
+                                          legislative_refs: tcc.legislative_refs.decorate,
+                                          thesis_refs: tcc.thesis_refs.decorate
+                                }
+    bibtex = TccDocument::ReferencesProcessor.new.execute(content)
+    bibtex
+  end
+
+end
