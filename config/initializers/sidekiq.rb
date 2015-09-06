ece5a76628f7670858f581e65ffec2848cd0f529
Roberto Cunha <robertosilvino@gmail.com>
2015-09-06 12:57:33 -0300

    alteração na validação do arquivo de configuração do sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,21 +2,18 @@ module Configuration
   def self.load!
     filename = File.join(Rails.root, 'config', 'sidekiq.yml')
 
-    unless File.file?(filename)
-      $stderr.puts 'Você precisa configurar corretamente os dados do sidekiq. Verifique o arquivo sidekiq.yml.example'
-      exit 1
-    end
+    if File.file?(filename)
 
     begin
       sidekiq_config = YAML::load_file(filename)[Rails.env]
     rescue ArgumentError
-      $stderr.puts "Seu arquivo de configuração do sidekiq, localizado em #{filename} não é um arquivo YAML válido."
-      exit 1
+
     end
 
     return if sidekiq_config.nil? # Não existe configurações definidas para este environment, então não vamos continuar
 
     Sidekiq::Logging.logger.level = sidekiq_config[:log_level] if sidekiq_config[:log_level]
+    end
   end
 end
 
