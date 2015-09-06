7fae7c44b807deec369dd33f0b7a33cfc6ac32ad
Roberto Cunha <robertosilvino@gmail.com>
2015-09-06 12:33:18 -0300

    alteração das mensagens de log

--- app/workers/batch_tccs_worker.rb
+++ app/workers/batch_tccs_worker.rb
@@ -7,7 +7,15 @@ class BatchTccsWorker
   end
 
   def perform(moodle_ids, user_name, user_email)
-    batch = BatchTccs.new
-    batch.generate_email(moodle_ids, user_name, user_email)
+    begin
+      logger.debug('>>> perform: Starting ')
+      batch = BatchTccs.new
+      logger.debug(">>> perform: BatchTccs.new = #{batch} ")
+      batch.generate_email(moodle_ids, user_name, user_email)
+      logger.debug('>>> perform: Ending ')
+      logger.info("+++ E-mail enviado para: <#{user_name}>#{user_email}")
+    rescue => e
+      logger.error "#{e.message}"
+    end
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,27 @@
+module Configuration
+  def self.load!
+    filename = File.join(Rails.root, 'config', 'sidekiq.yml')
+
+    unless File.file?(filename)
+      $stderr.puts 'Você precisa configurar corretamente os dados do sidekiq. Verifique o arquivo sidekiq.yml.example'
+      exit 1
+    end
+
+    begin
+      sidekiq_config = YAML::load_file(filename)[Rails.env]
+    rescue ArgumentError
+      $stderr.puts "Seu arquivo de configuração do sidekiq, localizado em #{filename} não é um arquivo YAML válido."
+      exit 1
+    end
+
+    return if sidekiq_config.nil? # Não existe configurações definidas para este environment, então não vamos continuar
+
+    Sidekiq::Logging.logger.level = sidekiq_config[:log_level] if sidekiq_config[:log_level]
+  end
+end
+
+Configuration.load!
+
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost:6379/0', namespace: 'unasus-tcc-production' }
 end
