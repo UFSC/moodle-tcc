f056db919e202d5a9683f9e10df1a577a11c18f8
Roberto Cunha <robertosilvino@gmail.com>
2015-10-03 14:00:10 -0300

    hint escondido e tooltip apresentando o conteúdo do hint: tcc_definition

--- app/assets/javascripts/tcc_definitions.js
+++ app/assets/javascripts/tcc_definitions.js
@@ -1,6 +1,11 @@
-//$(function () {
-//    $('[data-toggle="tooltip"]').tooltip()
-//    var tooltips = $("[title]").tooltip();
-//    tooltips.tooltip();
-//})
-//
+$(function() {
+    $(".help-block").each(function() {
+            $(this).hide();
+            label = $(this).parent().siblings('label');
+            label.attr("data-original-title", $(this).text());
+            label.attr("data-placement", "bottom");
+            tt_option = {delay: { "show": 750, "hide": 200 }};
+            label.tooltip(tt_option);
+        }
+    );
+});
