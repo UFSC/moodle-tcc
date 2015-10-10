c0a8bf8356bed96c073d63d80956551e316f393a
Roberto Cunha <robertosilvino@gmail.com>
2015-10-10 03:55:19 -0300

    ajuste na função e nos testes

--- spec/libs/controller_utils_spec.rb
+++ spec/libs/controller_utils_spec.rb
@@ -4,9 +4,63 @@ describe ControllersUtils do
   new_line = "\r\n\r\n"
   emptyParagraph = "<p></p>"
   emptyParagraphCKEditor = "<p style=\"text-align: justify;\"></p>"
-  contentTestNewLine = 'a'+new_line
-  contentTest = 'a'
+  contentTest = "<p style=\"text-align: justify;\">a</p>"
+  contentTestNewLine = contentTest+new_line
 
+  contents = [ "<p style=\"text-align: justify;\">a</p>",
+               "<p style=\"text-align: justify;\">qwert<img alt=\"\" src=\"/uploads/ckeditor/pictures/499/content_logo.png\" style=\"width: 452px; height: 623px;\" /></p>",
+               "<p style=\"text-align: justify;\"><strong>qwer</strong></p>",
+               "<p style=\"text-align: justify;\">a<strong>qwer</strong></p>",
+               "<blockquote>\r\n<p style=\"text-align: justify;\">a</p>\r\n</blockquote>",
+               "<ol>\r\n\t<li style=\"text-align: justify;\">a</li>\r\n</ol>",
+               "<ul>\r\n\t<li style=\"text-align: justify;\">a</li>\r\n</ul>",
+               "<p style=\"text-align: center;\">a</p>",
+               "<p style=\"text-align: justify; margin-left: 40px;\">a</p>",
+               "<p style=\"text-align: justify;\">a <citacao citacao-text=\"(SINTES, 2001)\" citacao_type=\"cd\" class=\"citacao-class\" contenteditable=\"false\" id=\"2981\" pagina=\"\" ref-type=\"livros\" reference_id=\"10910\" title=\"(SINTES, 2001)\">(SINTES, 2001)</citacao></p>",
+               "<table border=\"1\" cellpadding=\"1\" cellspacing=\"1\" style=\"width: 500px;\">\r\n\t<tbody>\r\n\t\t<tr>\r\n\t\t\t<td>\r\n\t\t\t<p>qwe</p>\r\n\t\t\t</td>\r\n\t\t\t<td>qwe</td>\r\n\t\t</tr>\r\n\t\t<tr>\r\n\t\t\t<td>\r\n\t\t\t<p>qwe</p>\r\n\t\t\t</td>\r\n\t\t\t<td>qwe</td>\r\n\t\t</tr>\r\n\t\t<tr>\r\n\t\t\t<td>\r\n\t\t\t<p>qwe</p>\r\n\t\t\t</td>\r\n\t\t\t<td>teste</td>\r\n\t\t</tr>\r\n\t</tbody>\r\n</table>"
+  ]
+
+  shared_examples_for 'remove blank lines with content' do
+    before(:each) do
+      @content = content
+      puts("Content: #{content}")
+      #Rails.logger.info("Content: #{content}")
+    end
+
+    it 'only the content' do
+      content_converted = ControllersUtils::remove_blank_lines(@content)
+      expect(content_converted).to eq(@content)
+    end
+
+    it 'the content and new line' do
+      content_converted = ControllersUtils::remove_blank_lines(@content+new_line)
+      expect(content_converted).to eq(@content)
+    end
+
+    it 'new line and content' do
+      content_converted = ControllersUtils::remove_blank_lines(new_line+@content)
+      expect(content_converted).to eq(@content)
+    end
+
+    it 'newline, content and new line' do
+      content_converted = ControllersUtils::remove_blank_lines(new_line+@content+new_line)
+      expect(content_converted).to eq(@content)
+    end
+
+    it 'newline, content, new line, content and new line' do
+      content_converted = ControllersUtils::remove_blank_lines(new_line+@content+new_line+@content+new_line)
+      expect(content_converted).to eq(@content+new_line+@content)
+    end
+
+  end
+
+  describe 'all content' do
+    contents.each do | content |
+      it_should_behave_like 'remove blank lines with content' do
+        let(:content) { content }
+      end
+    end
+  end
 
   shared_examples_for 'remove blank lines' do
     before(:each) do
@@ -92,5 +146,7 @@ describe ControllersUtils do
       let(:reserved_world) { emptyParagraphCKEditor+new_line }
     end
   end
+
+
 
 end
