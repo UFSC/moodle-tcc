7b794d7625ed0f9c67d20fb9432e7bd9043d712d
Roberto Silvino <robertosilvino@gmail.com>
2023-03-03 09:47:13 -0300

    Patch para state machine

---
+++ config/initializers/state_machine_patch.rb
@@ -0,0 +1,9 @@
+# config/initializers/state_machine_patch.rb
+# See https://github.com/pluginaweek/state_machine/issues/251
+module StateMachine
+  module Integrations
+    module ActiveModel
+      public :around_validation
+    end
+  end
+end
